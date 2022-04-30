e9008482379e5e550341792655d4e7f430ebaf93
Dicko Finus <dickofn@gmail.com>
2022-05-01 01:42:31 +0700

    feat: better search form

--- components/Pokemon/PokemonSearch.vue
+++ components/Pokemon/PokemonSearch.vue
@@ -3,7 +3,7 @@
     <div class="relative">
       <button
         type="submit"
-        class="absolute inset-y-0 left-4 z-20 cursor-pointer"
+        class="absolute inset-y-0 left-0 z-20 cursor-pointer pl-4 pr-2"
       >
         <SearchIcon class="h-4 w-4" />
       </button>
@@ -11,14 +11,22 @@
       <input
         id="output"
         v-model="search"
-        class="relative w-full rounded bg-white px-10 py-4 text-sm leading-none outline-none"
+        class="input relative w-full rounded border-primary bg-white py-4 pl-10 pr-14"
         type="text"
-        placeholder="Search..."
+        placeholder="Search by name or type..."
       />
 
       <button
-        onclick="eraseText();"
-        class="absolute inset-y-0 right-4 z-20 cursor-pointer text-xs font-medium leading-3 underline"
+        v-if="!filter"
+        class="link absolute inset-y-0 right-0 z-20 cursor-pointer pr-4 pl-2 text-xs font-medium leading-3 underline"
+        @click="submit"
+      >
+        Find
+      </button>
+      <button
+        v-else
+        class="link absolute inset-y-0 right-0 z-20 cursor-pointer pr-4 pl-2 text-xs font-medium leading-3 underline"
+        @click="clear"
       >
         Clear
       </button>
@@ -28,12 +36,22 @@
 
 <script setup>
 import { SearchIcon } from "@heroicons/vue/outline";
+import { storeToRefs } from "pinia";
+import { usePokemonStore } from "~~/store/pokemon";
 
 const search = ref("");
+
+const pokemonStore = usePokemonStore();
+const { filter } = storeToRefs(pokemonStore);
 
 const emit = defineEmits(["submit"]);
 
 function submit() {
-  emit("submit", search.value);
+  emit("submit", search.value.toLowerCase());
+}
+
+function clear() {
+  search.value = "";
+  emit("submit", search.value.toLowerCase());
 }
 </script>
