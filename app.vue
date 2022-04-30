799e86a112fad88cd32489acaa6128826e9b91eb
Dicko Finus <dickofn@gmail.com>
2022-04-30 13:14:43 +0700

    feat: test server function in vercel

--- app.vue
+++ app.vue
@@ -4,7 +4,12 @@
   </NuxtLayout>
 </template>
 
-<script setup>
+<script setup lang="ts">
+import { AllPokemonsResponse, PokemonResponse } from "./types/pokemon";
+import path from "path";
+import fs from "fs";
+import axios from "axios";
+
 useHead({
   title: "Welcome",
   titleTemplate: "%s | Dicko Pokedex",
@@ -23,6 +28,57 @@ useHead({
     },
   ],
 });
+
+const config = useRuntimeConfig();
+const { pokeApiUrl } = config.public;
+
+if (process.server) {
+  const pokemonsPath = path.resolve("public", "pokemons.json");
+  let file = null;
+  try {
+    file = fs.readFileSync(pokemonsPath);
+  } catch (error) {}
+  if (!file) {
+    fs.writeFileSync(pokemonsPath, JSON.stringify([]));
+    file = fs.readFileSync(pokemonsPath);
+  }
+
+  const pokemons = JSON.parse(file);
+  const resAllPokemons = await axios(`${pokeApiUrl}/pokemon`, {
+    params: { limit: 100000, offset: 0 },
+  });
+
+  const dataAllPokemons: AllPokemonsResponse = resAllPokemons.data;
+
+  const needUpdate = dataAllPokemons.results.length !== pokemons.length;
+  console.log("[LOG] | pokemons.length", pokemons.length);
+  console.log(
+    "[LOG] | dataAllPokemons.results.length",
+    dataAllPokemons.results.length
+  );
+
+  if (needUpdate) {
+    async function loadAllPokemons() {
+      const arr = pokemons;
+      try {
+        await Promise.all(
+          dataAllPokemons.results.map(async ({ name }) => {
+            try {
+              if (pokemons.find((i) => i.name === name)) return false;
+
+              const resPokemon = await axios(`${pokeApiUrl}/pokemon/${name}`);
+              const dataPokemon: PokemonResponse = resPokemon.data;
+              arr.push(dataPokemon);
+            } catch (error) {}
+          })
+        );
+      } catch (error) {}
+      return arr;
+    }
+    const loadedAllPokemons: PokemonResponse[] = await loadAllPokemons();
+    fs.writeFileSync(pokemonsPath, JSON.stringify(loadedAllPokemons));
+  }
+}
 </script>
 
 <style>
